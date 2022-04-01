ed9e8b1995cffc2aeb94938ebd6f42f6c3f4385e
APPMAN M-094 <admin@APPMAN-M-094.local>
2022-04-01 11:34:44 +0700

    Edit some field

--- app.py
+++ app.py
@@ -36,11 +36,19 @@ class CityModel(db.Model):
 
 db.create_all()
 
+# Request Parser
 city_add_args = reqparse.RequestParser()
 city_add_args.add_argument("name", type=str, required=True, help="The name of the city must be string")
 city_add_args.add_argument("temp", type=int, required=True, help="The value of the temp must be integer")
 city_add_args.add_argument("weather", type=str, required=True, help="The value of the weather must be string")
 city_add_args.add_argument("people", type=int, required=True, help="The number of people must be integer")
+
+# Update Request Parser
+city_update_args = reqparse.RequestParser()
+city_update_args.add_argument("name", type=str, required=True, help="Please enter your desired city")
+city_update_args.add_argument("temp", type=int, required=True, help="Please enter your desired temp")
+city_update_args.add_argument("weather", type=str, required=True, help="Please enter your desired weather")
+city_update_args.add_argument("people", type=int, required=True, help="Please enter your desired people")
 
 resource_field = {
     "id": fields.Integer,
@@ -74,6 +82,25 @@ class WeatherCity(Resource):
         db.session.add(city)
         db.session.commit()
         return city, 201
+
+    @staticmethod
+    @marshal_with(resource_field)
+    def patch(city_id):
+        args = city_update_args.parse_args()
+        result = CityModel.query.filter_by(id=city_id).first()
+        if not result:
+            abort(404, message=f"{city_id} not found")
+
+        if args["name"]:
+            result.name = args["name"]
+        if args["temp"]:
+            result.temp = args["temp"]
+        if args["weather"]:
+            result.weather = args["weather"]
+        if args["people"]:
+            result.people = args["people"]
+        db.session.commit()
+        return result
 
 
 # Call
